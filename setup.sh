b5b50b34be43cf5b8945ba06c8ff8153943ba58f
Fabricio <fabricio.reis@localhost.localdomain>
2024-06-07 23:33:01 -0300

    Updated files to grafana personalized

--- setup.sh
+++ setup.sh
@@ -12,3 +12,20 @@ oc set volumes deployment/example-application \
 docker build -t your-image-name .
 
 docker run -d -p 3000:3000 --name=grafana grafana/grafana-oss:latest
+
+podman tag quay.io/${RHT_OCP4_QUAY_USER}/do180-custom-httpd:v1.0
+podman push quay.io/${RHT_OCP4_QUAY_USER}/do180-custom-httpd:v1.0
+
+COMANDOS DOCKER:
+docker tag grafana-img:v1.0 quay.io/miami_freis/grafana-img-1.0
+
+docker push quay.io/miami_freis/grafana-img-1.0
+
+docker run -d -p 3000:3000 grafana-img:v1.0
+
+COMANDOS PODMAN:
+podman pull -q quay.io/miami_freis/grafana-img-1.0
+
+quay.io/miami_freis/grafana-img-1.0 
+
+podman run -d -p 3000:3000 localhost/grafana-img:v1.0
